a2167301c15f99805104badd6487a5b03244ab73
sandhya261996 <sandhyaupadhaya134@gmail.com>
2026-02-20 18:21:40 +0530

    add backend tf code

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
 terraform {
+
+    backend "s3" {
+    bucket = "tf-sandhya-bucket-terraform-state"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
